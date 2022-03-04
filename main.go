da9c74b3a4d2229c3a2a467975dacc8466abcdfe
Mark Oborne <markoborne1@gmail.com>
2022-03-04 22:59:59 +0000

    feat: add parallel rendering

--- main.go
+++ main.go
@@ -5,75 +5,95 @@ import (
 	"image/color"
 	"image/png"
 	"os"
+	"runtime"
 	"strconv"
+	"sync"
 
 	"github.com/alexflint/go-arg"
 )
 
 const (
-    width = 2560
-    height = 1440
+	width  = 2560
+	height = 1440
+	detail = 30
 )
 
 func mandelbrot(z, c complex128) complex128 {
-    new_real := real(z) * real(z) - imag(z) * imag(z) - real(c)
-    new_imag := real(z) * imag(z) * 2.0 + imag(c)
-    return complex(new_real, new_imag)
+	new_real := real(z)*real(z) - imag(z)*imag(z) - real(c)
+	new_imag := real(z)*imag(z)*2.0 + imag(c)
+	return complex(new_real, new_imag)
 }
 
 func escape_check(z complex128) bool {
-    return (real(z) * real(z) + imag(z) * imag(z)) > 4.0
+	return (real(z)*real(z) + imag(z)*imag(z)) > 4.0
 }
 
 func pixel_to_mandelbrot(pixel, offset, zoom float64, zoom_correction int) float64 {
-    return (pixel - float64(zoom_correction)) * zoom + offset
+	return (pixel-float64(zoom_correction))*zoom + offset
 }
 
-func render_mandelbrot(x_offset, y_offset, zoom float64, width, height int) *image.RGBA {
-    const MAX_ITER = 255 * 4
-    canvas := image.NewRGBA(image.Rect(0, 0, width, height))
+func render_mandelbrot(x_offset, y_offset, zoom float64, width, height int, wg *sync.WaitGroup) *image.RGBA {
+	const MAX_ITER = 255 * detail
+	width_chan := make(chan int)
+	defer close(width_chan)
+	canvas := image.NewRGBA(image.Rect(0, 0, width, height))
+	// dividing by two makes the zoom center on the middle of the screen
+	width_correction := width / 2
+	height_correction := height / 2
+	var canvas_mutex sync.Mutex
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func() {
+			wg.Add(1)
+			for x := range width_chan {
+				for y := 0; y < height; y++ {
+					cr := pixel_to_mandelbrot(float64(x), x_offset, zoom, width_correction)
+					ci := pixel_to_mandelbrot(float64(y), y_offset, zoom, height_correction)
+					c := complex(cr, ci)
+					z := complex(0.0, 0.0)
 
-    // dividing by two makes the zoom center on the middle of the screen
-    width_correction := width / 2
-    height_correction := height / 2
-    for i := 0; i < height; i++ {
-        for j := 0; j < width; j++ {
-            cr := pixel_to_mandelbrot(float64(j), x_offset, zoom, width_correction)
-            ci := pixel_to_mandelbrot(float64(i), y_offset, zoom, height_correction)
-            c := complex(cr, ci)
-            z := complex(0.0, 0.0)
-
-            num_of_iters := 0
-            for ; num_of_iters <= MAX_ITER; num_of_iters++ {
-                z = mandelbrot(z, c)
-                escaped := escape_check(z)
-                if escaped {
-                    break
-                }
-            }
-            colour := uint8(num_of_iters % 255)
-            canvas.SetRGBA(j, i, color.RGBA{ R: colour, G: colour, B: colour, A: 255 })
-        }
-    }
-    return canvas
+					num_of_iters := 0
+					for ; num_of_iters <= MAX_ITER; num_of_iters++ {
+						z = mandelbrot(z, c)
+						escaped := escape_check(z)
+						if escaped {
+							break
+						}
+					}
+					colour := uint8(num_of_iters % 255)
+					canvas_mutex.Lock()
+					canvas.SetRGBA(x, y, color.RGBA{R: colour, G: colour, B: colour, A: 255})
+					canvas_mutex.Unlock()
+				}
+			}
+			wg.Done()
+		}()
+	}
+	for i := 0; i < width; i++ {
+		width_chan <- i
+	}
+	return canvas
 }
 
 func main() {
-    var args struct {
-        Zoom string `default:"0.002"`
-        X_offset string
-        Y_offset string
-    }
-    arg.MustParse(&args)
-    zoom, err := strconv.ParseFloat(args.Zoom, 8)
-    x_offset, err := strconv.ParseFloat(args.X_offset, 8)
-    y_offset, err := strconv.ParseFloat(args.Y_offset, 8)
+	var args struct {
+		Zoom     string `default:"0.002"`
+		X_offset string
+		Y_offset string
+	}
+	arg.MustParse(&args)
+	zoom, err := strconv.ParseFloat(args.Zoom, 8)
+	x_offset, err := strconv.ParseFloat(args.X_offset, 8)
+	y_offset, err := strconv.ParseFloat(args.Y_offset, 8)
 
-    canvas := render_mandelbrot(x_offset, y_offset, zoom, width, height)
-    file, err := os.Create("result.png")
+	var wg sync.WaitGroup
+	canvas := render_mandelbrot(x_offset, y_offset, zoom, width, height, &wg)
+
+	file, err := os.Create("result.png")
 	if err != nil {
 		panic(err)
 	}
+	// ensure image has finished rendering before encoding
+	wg.Wait()
 	if err = png.Encode(file, canvas); err != nil {
 		panic(err)
 	}
